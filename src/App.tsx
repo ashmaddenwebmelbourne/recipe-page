39c80858e3142fba4f27360d0f1b0589e95640b3
Ash Madden <ashleymadden55@gmail.com>
2024-09-16 19:15:33 +1000

    Test images for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import imageTest from './assets/images/image-omelette.jpeg';
 
 function App() {
 	return (
 		<>
+			<img src={imageTest} />
 			<h1>Recipe Page</h1>
 		</>
 	);
